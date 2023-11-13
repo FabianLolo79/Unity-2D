c7e35e2f08b0cf7b353b6feb798f32f56afda37a
Fabián E. García <fabianegl79@gmail.com>
2023-11-13 18:04:45 -0300

    Revisar Raycast2D, implementado pero no funciona

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,6 +7,9 @@ public class Weapon : MonoBehaviour
     public GameObject bulletPrefab;
     public GameObject shooter;
 
+    public GameObject explosionEffect;
+    public LineRenderer lineRenderer;
+
     private Transform _firePoint;
 
     private void Awake()
@@ -47,6 +50,43 @@ public class Weapon : MonoBehaviour
                 //Right
                 bulletComponent.direction = Vector2.right; // new Vector2(1f, 0f)
             }
+        }
+    }
+
+    public IEnumerator ShootWithRaycast()
+    {
+        if(explosionEffect != null && lineRenderer != null)
+        {
+            RaycastHit2D hitInfo = Physics2D.Raycast(_firePoint.position, _firePoint.right);
+
+            if (hitInfo)
+            {
+                // Example code
+                // if(hitInfo.collider.tag == "Player")
+                //{
+                //    Transform player = hitInfo.transform;
+                //    player.GetComponent<PlayerHealth>().ApllyDamahe(5);
+                //}
+
+                // Instantiate explosion on hit point
+                Instantiate(explosionEffect, hitInfo.point, Quaternion.identity);
+
+                // Set Line renderer
+                lineRenderer.SetPosition(0, _firePoint.position);
+                lineRenderer.SetPosition(1, hitInfo.point);
+            }
+            else
+            {
+                lineRenderer.SetPosition(0, _firePoint.position);
+                lineRenderer.SetPosition(1, hitInfo.point + Vector2.right * 100);
+            }
+        
+
+            lineRenderer.enabled = true;
+
+            yield return null;
+
+            lineRenderer.enabled = false;
         }
     }
 }
